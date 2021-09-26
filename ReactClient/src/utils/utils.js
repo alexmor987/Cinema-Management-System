f8b0f3f6b298a1db43c8babd20bc0ec5e2b9d560
alexmor987 <alexmor987@gmail.com>
2021-09-26 22:16:10 +0300

    third commit

--- ReactClient/src/utils/utils.js
+++ ReactClient/src/utils/utils.js
@@ -1,13 +1,15 @@
 import axios from 'axios';
 import authSrv from '../services/auth';
 
+axios.interceptors.request.use(req=>
+    {
+        req.headers={"x-access-token":authSrv.getToken()};
+        return req;
+    })
+
 const getMovies=()=>
 {
-    let token=authSrv.getToken();
-    return  axios.get('http://localhost:5000/api/movies',
-            {
-                headers:{"x-access-token":token}
-            });
+    return  axios.get('http://localhost:5000/api/movies');
 }
 
 export default {getMovies}
